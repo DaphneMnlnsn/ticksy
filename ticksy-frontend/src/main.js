a3bc8f6893d87c129474ad6d8aa920a758ad6d78
kirstenqn <quianadomingo004@gmail.com>
2026-03-18 08:30:21 +0800

    Created Create Account Page

--- ticksy-frontend/src/main.js
+++ ticksy-frontend/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import CreateAccount from './Pages/CreateAccount.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+createApp(CreateAccount).mount('#app')
